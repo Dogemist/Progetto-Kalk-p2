ea04dc4e17115f981419ef05d9996d285167a042
Riccardo Dario <riuccc@gmail.com>
2018-09-04 13:25:14 +0200

    Fine Use Skill

--- SAUCE/Java/Use.java
+++ SAUCE/Java/Use.java
@@ -42,5 +42,66 @@ public class Use {
         else
             System.out.print("I due attacchi sono diversi\n");
 
+        resb=b[0].sum(b[1]);
+        System.out.print("La somma dei due attacchi è: "+resb+"\n");
+
+        resb=b[0].dif(b[1];
+        System.out.print("La differenza dei due attacchi è: "+resb+"\n");
+
+        //OPERAZIONI SKILL
+        int mana=600;
+
+        check=s[0].isCastable(time, distance);
+        System.out.print("La skill "+s[0].getName+" è castabile in "+time+" secondi\n";
+
+        check=s[0].isCastable(time, distance,mana);
+        System.out.print("La skill "+s[0].getName+" è castabile in "+time+" secondi con "+mana+" mana\n";
+
+        resb=s[0].DPS(distance);
+        System.out.print("La skill fa "+resb+" DPS\n");
+
+        resb=s[0].DamageByTime(time,distance);
+        System.out.print("La skill fa "+resb+" danni in "+time+" secondi\n");
+
+        res=s[0].HitByTime(time,distance);
+        System.out.print("La skill fa "+res+" colpi in "+time+" secondi\n");
+
+        resb=s[0].totalTime(distance);
+        System.out.print("La skill colpisce in "+resb+" secondi\n");
+
+        resb=s[0].getAnimation();
+        res=s[0].getCooldown();
+        System.out.print("La skill"+s[0].getName()+" ha una animazione di "+resb" secondi, un cooldown di "+res+" secondi e costa "+s[0].getMana()+" mana\n");
+
+        s[0].setReady(true);
+        check=s[0].getReady();
+        if(check)
+            System.out.print("La skill e pronta\n");
+        else
+            System.out.print("La skill non e pronta\n");
+
+        check=s[0].equals(s[0]);
+        if(check)
+            System.out.print("Le skill "+s[0].getName()+" e "+s[0].getName()+" sono uguali\n");
+        else
+            System.out.print("Le skill "+s[0].getName()+" e "+s[0].getName()+" sono diverse\n");
+
+        resb=s[0].sum(s[1]);
+        System.out.println("Le skill "+s[0].getName()+" e "+s[0].getName()+" hanno somma: "+resb);
+
+        resb=s[0].dif(s[1]);
+        System.out.println("Le skill "+s[0].getName()+" e "+s[0].getName()+" hanno differenza: "+resb);
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
